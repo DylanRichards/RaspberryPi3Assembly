ee0f862d912e0bf44a2a5ef9ac23da486a70ac58
Dylan Richards <dylan.richards81@gmail.com>
2020-09-27 23:28:47 +0000

    Updated Chapter 1

--- chapter01/first.s
+++ chapter01/first.s
@@ -3,7 +3,6 @@
 .global main /* 'main' is our entry point and must be global */
 .func main   /* 'main' is a function */
 
-main:          /* This is main */
-    mov r0, #2 /* Put a 2 inside the register r0 */
-    bx lr      /* Return from main */
-
+main:           /* This is main */
+    mov x0, #2  /* Put a 2 inside the register x0 */
+    ret         /* Return from main */
